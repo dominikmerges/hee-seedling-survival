6df8c2628ec9a427a6312636cb157a0ca1f196f8
Ken Kellner <ken.kellner@gmail.com>
2014-06-21 14:38:57 -0400

    Add seedling growth analysis.

--- analysis_seedling_growth.R
+++ analysis_seedling_growth.R
@@ -1,32 +1,14 @@
-
-
-
-#Analyses Ideas
-
-#1. Seedling survival
-#     Random Covariates:
-#     - Site (n=15)
-#     - Plot (n=54)
-#     Fixed covariates:
-#     - Distance to edge (?), how to deal with shelterwoods?
-#     - Species
-#     - Source (0-0 or 1-0)
-#     - Initial height (z-score separated by source)
-#     - Canopy cover done
-#     - Competition (stem density?) done
-#     - Browse at time t-1 done
-#     - Time since establishment done
-#
-
-#3. Browse intensity (how to calculate?)
+#2. Seedling growth (height)
 #     Random Covariates:
 #     - Site, plot
 #     Fixed Covariates:
-#     - Distance to edge
-#     - Treatment?
-#     - Plant density
-#     - Use (pellet counts)
-#     - Age of opening
+#     - Canopy cover
+#     - Browse at time t-1
+#     - Height at time t-1
+#     - Competition
+#     - Species
+#     - Source
+#
 
 source('format_data.R')
 
@@ -36,17 +18,48 @@ seedling <- format.seedling('seedlingmaster.csv')
 #Only keep seedlings that "established" and were not in shelterwoods
 keep <- which(seedling$surv.sprout[,1]==1&seedling$seedling.data$plotid<49)
 
-#Response variable
-surv <- seedling$surv.sprout[keep,]
+sprout.raw <- seedling$sprout[keep,]
 
-nsamples <- numeric(dim(surv)[1])
-for (i in 1:dim(surv)[1]){
-  nsamples[i] <- length(na.omit(surv[i,]))
+for (i in 1:dim(sprout.raw)[1]){
+  hold <- sprout.raw[i,]
+  if(1%in%hold){
+    start <- min(which(hold==1),na.rm=TRUE)
+    sprout.raw[i,start:dim(sprout.raw)[2]] <- 1
+  }
+}
+#Select only measurement samples
+sprout.raw <- sprout.raw[,c(2,4,6)]
+end <- numeric(dim(sprout.raw)[1])
+for (i in 1:dim(sprout.raw)[1]){
+  hold <- sprout.raw[i,]
+  if(1%in%hold){
+    end[i] <- min(which(hold==1),na.rm=TRUE) - 1
+  } else {end[i]=3}
 }
 
+surv <- seedling$surv[keep,c(2,4,6)]
+endsurv <- numeric(dim(surv)[1])
+for (i in 1:dim(surv)[1]){
+  hold <- surv[i,]
+  if(0%in%hold){
+    endsurv[i] <- min(which(hold==0),na.rm=TRUE) - 1
+  } else {endsurv[i]=3}
+}
+
+last <- pmin(end,endsurv)
+keep2 <- which(last>0)
+
+growth <- seedling$htgrowth[keep,]
+growth <- growth[keep2,]
+
+nsamples <- last[keep2]
+last <-last[keep2]
+
+
 #Seedling-level covariates
-nseedlings <- dim(surv)[1]
+nseedlings <- dim(growth)[1]
 seedling.covs <- seedling$seedling.data[keep,]
+seedling.covs <- seedling.covs[keep2,]
 seed.sitecode <- seedling.covs$siteid
 seed.plotcode <- seedling.covs$plotid
 age <- seedling.covs$age
@@ -55,13 +68,15 @@ species <- seedling.covs$species
 
 #Browse - simplify to presence/absence for now
 browse <- seedling$browse[keep,]
+browse <- browse[keep2,]
+browse <- as.matrix(cbind(browse[,1]+browse[,2],browse[,3]+browse[,4],browse[,5]+browse[,6]))
 browse[which(browse>1,arr.ind=TRUE)] = 1
 
 #Browse quality control
 for (i in 1:nseedlings){
-  for (j in 2:nsamples[i]){
-    if(!is.na(surv[i,j])&&is.na(browse[i,j-1])){
-      browse[i,j-1] <- 0
+  for (j in 1:last[i]){
+    if(is.na(browse[i,j])){
+      browse[i,j] <- 0
     }
   }
 }
@@ -76,26 +91,18 @@ canopy[4] <- 0
 plot.sitecode <- seedling$plot.data$siteid
 
 #Competition
-comp <- seedling$comp.data[,1,]
-herb <- seedling$comp.data[,2,]
+comp <- seedling$comp.data[,1,][,c(1,3,5,7)]
+herb <- seedling$comp.data[,2,][,c(1,3,5,7)]
 comp[which(is.na(comp),arr.ind=TRUE)] <- 0
 herb[which(is.na(herb),arr.ind=TRUE)] <- 0
 
 #Site level variables
 nsites <- 12
-elapsed.raw <- as.matrix(seedling$elapsed)
-elapsed <- (elapsed.raw - mean(elapsed.raw))/sd(elapsed.raw)
 
-#Season vector
-#summer = 1
-season <- c(1,0,1,0,1,0,1,0)
-
-#Index
-
-cucount = matrix(NA, nseedlings, 8)
+cucount = matrix(NA, nseedlings, 4)
 index=1
 for(i in 1:nseedlings){
-  for(j in 2:nsamples[i]){
+  for(j in 1:last[i]){
     cucount[i,j] = index
     index = index+1
   }}
@@ -104,31 +111,29 @@ for(i in 1:nseedlings){
 
 #Bundle data for JAGS
 
-jags.data <- c('surv','nseedlings','nsamples','nplots','nsites','cucount'
+jags.data <- c('growth','nseedlings','nsamples','nplots','nsites','cucount'
                ,'seed.plotcode','plot.sitecode','seed.sitecode'
                #seedling covariates
                ,'age','start.height','browse','species'
                #plot covariates
                ,'distance','aspect','canopy','comp','herb'
-               #site covariates
-               ,'elapsed','season'
-               )
+)
 
 ################################
 
 #Model file
 
-modFile <- 'models/model_seedling_survival.R'
+modFile <- 'models/model_seedling_growth.R'
 
 ################################
 
 #Parameters to save
 
-params <- c('grand.sd','plot.sd'
-            ,'b.browse','b.herb','b.canopy','b.comp','b.distance','b.aspect','b.elapsed'
-            ,'b.species','b.age','b.browse','b.height','b.season'
+params <- c('grand.sd','plot.sd','ind.sd'
+            ,'b.browse','b.herb','b.canopy','b.comp','b.distance','b.aspect'
+            ,'b.species','b.age','b.browse','b.height'
             ,'fit','fit.new'
-  )
+)
 
 ################################
 
@@ -136,9 +141,5 @@ params <- c('grand.sd','plot.sd'
 
 require(jagsUI)
 
-surv.output <- jags(data=jags.data,parameters.to.save=params,model.file=modFile,
+growth.output <- jags(data=jags.data,parameters.to.save=params,model.file=modFile,
                     n.chains=3,n.iter=1000,n.burnin=500,n.thin=2,parallel=TRUE)
-
-
-
-
---
+++ models/model_seedling_growth.R
@@ -0,0 +1,63 @@
+
+model {
+  
+  #Likelihood
+  
+  for (i in 1:nsites){
+    site.mean[i] ~ dnorm(grand.mean,grand.tau)
+  }
+  
+  for (i in 1:nplots){
+    plot.mean[i] ~ dnorm(plot.pred[i], plot.tau)
+    plot.pred[i] <- site.mean[plot.sitecode[i]] 
+                  + b.canopy*canopy[i]
+                  + b.distance*distance[i] + b.aspect*aspect[i]
+                  
+  }
+  
+  for (i in 1:nseedlings){
+    for (j in 1:nsamples[i]){
+      
+      growth[i,j] ~ dnorm(mu[i,j],ind.tau)
+      
+      mu[i,j] <- plot.mean[seed.plotcode[i]] 
+                      + b.species*species[i] + b.age*age[i] + b.height*start.height[i]
+                      + b.browse*browse[i,j]
+                      + b.comp*comp[seed.plotcode[i],j] + b.herb*herb[seed.plotcode[i],j]
+                      
+      
+      res[cucount[i,j]] <- growth[i,j] - mu[i,j]
+      sqres[cucount[i,j]] <- pow(res[cucount[i,j]],2)
+      growth.new[i,j] ~ dnorm(mu[i,j],ind.tau)
+      res.new[cucount[i,j]] <- growth.new[i,j] - mu[i,j]
+      sqres.new[cucount[i,j]] <- pow(res.new[cucount[i,j]],2)
+    }
+  }
+  
+  fit <- sum(sqres[])
+  fit.new <- sum(sqres.new[])
+  
+  #Priors
+  
+  grand.mean ~ dunif(-100,100)
+  grand.tau <- pow(grand.sd,-2)
+  grand.sd ~ dunif(0,100)
+  
+  plot.tau <- pow(plot.sd,-2)
+  plot.sd ~ dunif(0,100)
+  
+  ind.tau <- pow(ind.sd,-2)
+  ind.sd ~ dunif(0,100)
+  
+  b.herb ~ dnorm(0,0.01)
+  b.canopy ~ dnorm(0,0.01)
+  b.comp ~ dnorm(0,0.01)
+  b.distance ~ dnorm(0,0.01)
+  b.aspect ~ dnorm(0,0.01)
+  
+  b.species ~ dnorm(0,0.01)
+  b.age ~ dnorm(0,0.01)
+  b.browse ~ dnorm(0,0.01)
+  b.height ~ dnorm(0,0.01)
+  
+}
